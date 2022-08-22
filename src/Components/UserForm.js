d7734792adea1b024c61c5a8b2d350ddc6adb2e6
victhall <victoriahall.dev@gmail.com>
2022-08-22 11:43:29 -0400

    implement userform component

---
+++ src/Components/UserForm.js
@@ -0,0 +1,23 @@
+export default function UserForm() {
+  return (
+    <form>
+
+      <div>
+
+        <div>
+          <label>Username</label>
+          <input
+            type='text' />
+        </div>
+
+        <div>
+          <label>Age (Years)</label>
+          <input
+            type='number' />
+        </div>
+
+      </div>
+
+    </form>
+  )
+}
