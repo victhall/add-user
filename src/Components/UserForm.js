be61ba4c14e1cdba1e808b7db155a53bdf6d0c14
victhall <victoriahall.dev@gmail.com>
2022-08-22 18:02:06 -0400

    create handlers for form submission

--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -1,23 +1,53 @@
-export default function UserForm() {
-  return (
-    <form>
+import { useState } from "react"
 
-      <div>
+export default function UserForm() {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const nameChangeHandler = function (event) {
+    setEnteredUsername(event.target.value)
+  }
+
+  const ageChangeHandler = function (event) {
+    setEnteredAge(event.target.value)
+  }
+
+  const submitHandler = function (event) {
+    ProgressEvent.preventDefault()
+
+    const userData = {
+      username: enteredUsername,
+      age: enteredAge
+    };
+
+    setEnteredUsername('')
+    setEnteredAge('')
+  }
+
+
+  return (
+    <form >
+
+      <div onSubmit={submitHandler}>
 
         <div>
           <label>Username</label>
           <input
-            type='text' />
+            type='text'
+            value={enteredUsername}
+            onChange={nameChangeHandler} />
         </div>
 
         <div>
           <label>Age (Years)</label>
           <input
-            type='number' />
+            type='number'
+            value={enteredAge}
+            onChange={ageChangeHandler} />
         </div>
 
       </div>
-
+      <button type="submit">Add User</button>
     </form>
   )
 }
