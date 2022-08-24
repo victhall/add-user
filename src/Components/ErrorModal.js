8e8ef028220283854ac55eb0cd05976bdec2167d
victhall <victoriahall.dev@gmail.com>
2022-08-24 12:28:41 -0400

    restructure error modal as portal

--- src/Components/ErrorModal.js
+++ src/Components/ErrorModal.js
@@ -13,3 +13,30 @@ export default function ErrorModal(props) {
   </footer>
   </div>
 }
+//Modal portal
+
+// import Button from "./Button"
+// import ReactDOM from 'react-dom'
+
+// const modalOverlay = function (props) {
+//   return <>
+//     <header>
+//       <h2>{props.title}</h2>
+//     </header>
+//     <div>
+//       <p>{props.message}</p>
+//     </div>
+//     <footer>
+//       <Button onClick={props.onConfirm}>Okay</Button>
+//     </footer>
+//   </>
+// }
+
+// export default function ErrorModal(props) {
+//   return <>
+//     {ReactDOM.createPortal(<modalOverlay
+//       title={props.title}
+//       message={props.message}
+//       onClick={props.onConfirm} />, document.getElementById('overlay-root'))}
+//   </>
+// }
